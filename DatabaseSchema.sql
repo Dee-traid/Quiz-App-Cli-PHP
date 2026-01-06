8fb1c8547411379932e7eb6700785611b9e062da
Adejoh Joshua <adekelvin369@gmail.com>
2026-01-06 14:29:21 +0100

    Revise database schema with new tables and fields

--- DatabaseSchema.sql
+++ DatabaseSchema.sql
@@ -1,18 +1,33 @@
-Create table admins(
-    id varchar(100) primary key not null,
-    email varchar(100) not null,
-    password varchar(5000)  
+CREATE TABLE admins(
+    id VARCHAR(100) PRIMARY KEY NOT NULL,
+    fullname VARCHAR(225) NOT NULL,
+    password VARCHAR(225) NOT NULL,
+    email VARCHAR(225) NOT NULL
 );
 
-Create table users(
-
+CREATE TABLE users(
+    id VARCHAR(100) PRIMARY KEY NOT NULL,
+    fullname VARCHAR(225) NOT NULL,
+    password VARCHAR(225) NOT NULL,
+    email VARCHAR(225) NOT NULL,
+    regNo  VARCHAR (10)
 );
 
-Create table question_bank(
-    id varchar(100) primary key not null,
-    course varchar(225) not null,
-    subject varchar(225) not null,
-    question varchar(225) not null, 
-    option JSON,
-    answer int
+
+CREATE TABLE question_bank(
+    id VARCHAR(100) PRIMARY KEY NOT NULL,
+    course VARCHAR(225) NOT NULL,
+    subject VARCHAR(225) NOT NULL,
+    question VARCHAR(500) NOT NULL,
+    options JSON,
+    answer INT
+);
+
+CREATE TABLE  quiz_results(
+    id VARCHAR(100) PRIMARY KEY NOT NULL,
+    user_id VARCHAR(100) NOT NULL,
+    subject VARCHAR(225) NOT NULL,
+    score INT,
+    quiz_taken_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id)  REFERENCES users(id)
 );
